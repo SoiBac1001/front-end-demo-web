0c3c22b45def29fbabae0096627b82633414be39
LDAP\nvbao2 <baonv1001@gmail.com>
2021-10-20 18:29:46 +0700

    dev: study angular day 5

--- src/app/hi/hi.component.ts
+++ src/app/hi/hi.component.ts
@@ -14,6 +14,25 @@ export class HiComponent implements OnInit, OnDestroy {
     age: 18,
   };
 
+  authors = [
+    {
+      id: 1,
+      firstName: 'Flora',
+      lastName: 'Twell',
+      email: 'ftwell0@phoca.cz',
+      gender: 'Female',
+      ipAddress: '99.180.237.33',
+    },
+    {
+      id: 2,
+      firstName: 'Priscella',
+      lastName: 'Signe',
+      email: 'psigne1@berkeley.edu',
+      gender: 'Female',
+      ipAddress: '183.243.228.65',
+    },
+  ];
+
   ngOnInit() {
     console.log('Hi init');
   }
